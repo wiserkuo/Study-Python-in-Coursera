bf409b0275bbd18183b05f5b0ab953846043f655
thomas <thomasc@mitac.com.tw>
2016-03-15 16:11:20 +0800

    [modified] Add a comment and change a description of setup.py

--- python_call_c/setup.py
+++ python_call_c/setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
+# just see the links of README.md for how to fill in those blank.
 module1 = Extension('example',
                     sources = ['wrap.c', 'example.c'])
 
-setup (name = 'example',
-       version = '1.0',
-       description = 'example',
-       ext_modules = [module1])
+setup(name = 'example',
+      version = '1.0',
+      description = 'description of example.',
+      ext_modules = [module1])
